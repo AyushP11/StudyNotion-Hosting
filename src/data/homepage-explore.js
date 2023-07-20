fc0440cf913cb9f86c488a9b2f86eebe7eceda37
AyushP11 <ayushpanchakshari@gmail.com>
2023-07-20 13:13:44 +0530

    First Commit of StudyNotion

---
+++ src/data/homepage-explore.js
@@ -0,0 +1,117 @@
+export const HomePageExplore = [
+    {
+        tag: 'Free',
+        courses : [
+            {
+                heading : "Learn HTML",
+                description : "This course covers the basic concepts of HTML including creating and structuring web pages, adding text, links, images, and more.",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+            {
+                heading : "Learn CSS",
+                description : "This course explores advanced topics in HTML5 and CSS3, including animations, transitions, and layout techniques",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+            {
+                heading : "Responsive Web design",
+                description : "This course teaches responsive web design techniques, allowing web pages to adapt to different devices and screen sizes",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+        ]
+    },
+    {
+        tag: 'New to coding',
+        courses : [
+            {
+                heading : "HTML",
+                description : "This course covers the basic concepts of HTML including creating and structuring web pages, adding text, links, images, and more.",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+            {
+                heading : "CSS",
+                description : "This course explores advanced topics in HTML5 and CSS3, including animations, transitions, and layout techniques",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+            {
+                heading : "Responsive ",
+                description : "This course teaches responsive web design techniques, allowing web pages to adapt to different devices and screen sizes",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+        ]
+    },
+    {
+        tag: 'Most popular',
+        courses : [
+            {
+                heading : "Java",
+                description : "This Java course provides a thorough understanding of the language, covering basic syntax, object-oriented programming, data structures, algorithms and develop practical skills.",
+                level : 'Intermediate',
+                lessionNumber : 6
+            },
+            {
+                heading : "Python",
+                description : "A practical Python course that equips learners with the necessary skills to write efficient code, handle data, automate tasks, and develop applications.",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+            {
+                heading : "SCSS",
+                description : "This SCSS course offers hands-on training for mastering features of SCSS, empowering learners to create scalable CSS stylesheets for modern web development.",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+        ]
+    },
+    {
+        tag: 'Skills paths',
+        courses : [
+            {
+                heading : "Flask",
+                description : "This Flask course teaches learners how to build web applications using Python and the Flask framework, covering routing, templates, database integration, and deployment.",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+            {
+                heading : "Django",
+                description : "The Django course provides comprehensive training on building web applications using Python and the Django framework, covering models, views, templates, authentication, and database integration.",
+                level : 'Intermediate',
+                lessionNumber : 6
+            },
+            {
+                heading : "Fast API",
+                description : "The FastAPI course offers hands-on training to master the FastAPI framework for building high-performance web APIs using Python, including features like automatic documentation and asynchronous support.",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+        ]
+    },
+    {
+        tag: 'Career paths',
+        courses : [
+            {
+                heading : "Next.JS",
+                description : "The Next.JS course provides practical training on building modern, server-rendered React applications with enhanced performance, SEO capabilities, and seamless client-side rendering.",
+                level : 'Intermediate',
+                lessionNumber : 6
+            },
+            {
+                heading : "Nuxt.JS",
+                description : "The Nuxt.JS course offers comprehensive training on building powerful and scalable Vue.js applications with server-side rendering, routing, state management, and optimized SEO capabilities.",
+                level : 'Intermediate',
+                lessionNumber : 6
+            },
+            {
+                heading : "Sanity",
+                description : "The Sanity course provides hands-on training for developers to effectively use Sanity.io, a powerful headless CMS, to build and manage content-driven applications with flexibility and ease.",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+        ]
+    },
+]
